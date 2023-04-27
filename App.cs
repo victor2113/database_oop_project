d6fd74e7654910e9ea8ecf9bf297affe777b96c1
Илья Игнатов <80439643+victor2113@users.noreply.github.com>
2023-04-27 19:29:28 +1000

    Сделал регистарацию нового юзера

--- App.cs
+++ App.cs
@@ -10,16 +10,17 @@ namespace database_oop_project
 {
     public class App
     {
-        DataBase database = new DataBase(); 
+        DataBase database = new DataBase();
         public void Begin()
         {
+            
             Console.CursorVisible = false;
             string promt = @"
 Use the arrow keys to choose options and press enter to select one";
 
 
 
-            string[] options = { "Registration", "Registration", "Exit" };
+            string[] options = { "Sign_in", "Sign_up", "Exit" };
             StartMenu startMenu = new StartMenu(options, promt);
             int selectedIndex = startMenu.Run();
 
@@ -29,10 +30,10 @@ Use the arrow keys to choose options and press enter to select one";
             switch (selectedIndex)
             {
                 case 0:
-                    Registration();
+                    Sign_in();
                     break;
                 case 1:
-                    Registration();
+                    Sign_up();
                     break;
                 case 2:
                     Exit();
@@ -51,18 +52,20 @@ Use the arrow keys to choose options and press enter to select one";
             Console.ReadKey(true);
             Environment.Exit(0);
         }
-        private void Registration()
+        private void Sign_in()
         {   
             string userLogin = "";
             string userPassword = "";
-            
+
+           
+
 
             SqlDataAdapter adapter = new SqlDataAdapter();
             DataTable table = new DataTable();
             
             
             Console.Clear();
-            Console.WriteLine("Enter ;ogin");
+            Console.WriteLine("Enter login");
             while (true)
             {
                 ConsoleKeyInfo c = Console.ReadKey();
@@ -103,7 +106,8 @@ Use the arrow keys to choose options and press enter to select one";
 
 
 
-            string query_string = $"select id_user , user_login , user_password from register where user_login = '{userLogin}' and user_password = '{userPassword}'";
+            string query_string =
+            $"select id_user , user_login , user_password from register where user_login = '{userLogin}' and user_password = '{userPassword}'";
             SqlCommand command = new SqlCommand(query_string , database.getConnection());
 
             adapter.SelectCommand = command;
@@ -121,14 +125,108 @@ Use the arrow keys to choose options and press enter to select one";
             Begin();
         }
 
-
-        private void RunTheGame()
+        private void Sign_up()
         {
+            string userLogin = "";
+            string userPassword = "";
+            database.openConnection();
             Console.Clear();
-            Console.WriteLine("bla bla");
-            Console.ReadKey(true);
-            Begin();
+            Console.WriteLine("Enter login");
+            while (true)
+            {
+                ConsoleKeyInfo c = Console.ReadKey();
+                Console.Clear();
+                if (c.Key == ConsoleKey.Enter)
+                    break;
+                if (c.Key == ConsoleKey.Backspace)
+                {
+                    if (userLogin.Length > 0)
+                        userLogin = userLogin.Remove(userPassword.Length - 1);
+                }
+                else
+                    userLogin += c.KeyChar;
+                Console.Write(userLogin);
+
+            }
+            Console.Clear();
+            Console.WriteLine("Enter pass");
+            while (true)
+            {
+                ConsoleKeyInfo c = Console.ReadKey();
+                Console.Clear();
+                if (c.Key == ConsoleKey.Enter)
+                    break;
+                if (c.Key == ConsoleKey.Backspace)
+                {
+                    if (userPassword.Length > 0)
+                        userPassword = userPassword.Remove(userPassword.Length - 1);
+                }
+                else
+                    userPassword += c.KeyChar;
+                foreach (char ch in userPassword)
+                    Console.Write('*');
+            }
+            Console.WriteLine("Login: {0}", userLogin);
+            Console.WriteLine("Pass: {0}", userPassword);
+
+
+
+            if(existingUser(userLogin, userPassword))
+            {
+                string query_string =
+            $"insert into register(user_login , user_password)  values('{userLogin}','{userPassword}')";
+
+                SqlCommand command = new SqlCommand(query_string, database.getConnection());
+
+
+
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    Console.WriteLine("success");
+                }
+                else
+                {
+                    Console.WriteLine("error!");
+                }
+                database.closeConnection();
+
+                Console.ReadKey(true);
+                Sign_in();
+            }
+            else { 
+                Console.WriteLine("acc already exist!");
+                Console.ReadKey(true);
+                Begin();
+            }
+
+            
         }
-       
+        
+       private Boolean existingUser(string login,string password)
+        {
+            string userLogin = "";
+            string userPassword = "";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string query_string =
+           $"select id_user , user_login , user_password from register where user_login = '{userLogin}' and user_password = '{userPassword}'";
+            SqlCommand command = new SqlCommand(query_string, database.getConnection());
+
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
