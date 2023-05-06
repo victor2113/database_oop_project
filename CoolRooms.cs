a39ae152d540a3b2931939517f27a8045d77ae33
elyamikhailova <103018160+elyamikhailova@users.noreply.github.com>
2023-05-06 21:35:17 +1000

    Рабочие регистрация и вход

---
+++ CoolRooms.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_oop_project
+{
+    public class CoolRooms
+    {
+        public void Entry()
+        {
+            Console.CursorVisible = false;
+            string promt = @"
+Use the arrow keys to choose room and press enter to select one";
+
+            DateTime date1 = DateTime.Today;
+
+            string[] options = { "Red Room", "Kid Room", "Console Room" };
+            StartMenu startMenu = new StartMenu(options, promt);
+            int selectedIndex = startMenu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    RedRoom();
+                    break;
+                case 1:
+                    KidRoom();
+                    break;
+                case 2:
+                    ConsoleRoom();
+                    break;
+            }
+        }
+        private void RedRoom()
+        {
+
+        }
+
+        private void KidRoom()
+        {
+
+        }
+
+        private void ConsoleRoom()
+        {
+
+        }
+    }
+}
